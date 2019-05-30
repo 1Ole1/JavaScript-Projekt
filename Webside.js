3116793738d6b0b7a95e0df8220b3efc7d73186d
1Ole1 <46760194+1Ole1@users.noreply.github.com>
2019-05-30 13:58:56 +0200

    Create Webside.js
    Javascript Zum Üben

---
+++ Webside.js
@@ -0,0 +1,7 @@
+Check = prompt('Geben Sie Ihr Passwort für diese Seite ein', '');
+if (Check != 'Rumpelstielz') {
+  alert('Du kommst hier nicht rein!');
+} else {
+  alert('Sie haben Zutritt');
+}
+}
